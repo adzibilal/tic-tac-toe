0f941911d1c2a5f83e888b8762515b59f748360a
adzibilal <adzibilal02@gmail.com>
2024-04-21 15:13:24 +0700

    first commit

---
+++ src/main/java/com/adzibilal/tictactoe/Tictactoe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.adzibilal.tictactoe;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Tictactoe {
+
+    public static void main(String[] args) {
+       UITicTacToe tictactoe = new UITicTacToe();
+    }
+}
